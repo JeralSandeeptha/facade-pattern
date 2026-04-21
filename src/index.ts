8d284e03f830c26ab21fca8a0e6ea4e1d8548d32
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2026-04-21 21:00:00 +0530

    added strategy pattern

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('App is working');
+import { JiraStrategy } from "./classes/JiraStrategy.js";
+import { TaskManagerStategy } from "./classes/TaskManagerStategy.js";
+import { WrikeStrategy } from "./classes/WrikeStrategy.js";
+
+// create strategy
+const currentStrategy = new JiraStrategy();
+
+// create the task manager
+const taskManager = new TaskManagerStategy(currentStrategy);
+
+taskManager.login();
+taskManager.createTask();
+
+// change the strategy (change the platform)
+const newStrategy = new WrikeStrategy();
+taskManager.setStrategy(newStrategy);
+
+// still we are using same functions but behaviour is different and unique according to each platforms
+taskManager.login();
+taskManager.createTask();
